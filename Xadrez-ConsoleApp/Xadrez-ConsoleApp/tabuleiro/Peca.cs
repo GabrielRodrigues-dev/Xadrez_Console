bd04cac483de43e2f5b73932f542c69994bbff20
gabriel henrique rodrigues ribeiro costa <gabrielup14@gmail.com>
2023-10-21 09:28:13 -0300

    criado cor, peca, e tabuleiro

---
+++ Xadrez-ConsoleApp/Xadrez-ConsoleApp/tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, int qteMovimentos, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+            this.tab = tab;
+        }
+    }
+}
